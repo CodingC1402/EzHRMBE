de0676a422bdd2f1e5cc905b6b1998cdf2b8ab00
feelings93 <19521298@gm.uit.edu.vn>
2022-04-05 14:20:35 +0700

    Modify cors for cookie issue in client and make route get profile

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import userRouter from './routes/userRouter';
 import cors from 'cors';
 import Env from './configurations/Env';
 import session from 'express-session';
-import MongoStore from 'connect-mongo'
+import MongoStore from 'connect-mongo';
 import authenticateRouter from './routes/authenticateRouter';
 import { SESSION_EXPIRE_SPAN } from './configurations/Security';
 import bcrypt from 'bcrypt';
@@ -14,38 +14,40 @@ require('dotenv/config');
 const server = express();
 
 // Server setups
-server.use(cors());
+server.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 server.use(express.json());
-server.use(session({
-  secret: Env.TOKEN_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: Env.DB_CONNECTION,
-    collectionName: "connectSessions"
-  }),
-  cookie: { 
-    secure: false,
-    maxAge: SESSION_EXPIRE_SPAN,
-    sameSite: 'strict'
-  }
-}))
+server.use(
+  session({
+    secret: Env.TOKEN_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: Env.DB_CONNECTION,
+      collectionName: 'connectSessions',
+    }),
+    cookie: {
+      secure: false,
+      maxAge: SESSION_EXPIRE_SPAN,
+      sameSite: 'strict',
+    },
+  })
+);
 
 //Routers
 server.use(authenticateRouter);
 server.use('/user', userRouter);
 
 server.get('/', (req, res) => {
-  res.send("Hello world");
-})
+  res.send('Hello world');
+});
 
 server.listen(42069);
 
 // DB connection
 let dbConnect = Env.DB_CONNECTION;
 mongoose.connect(dbConnect, () => {
-  console.log("Connected to MongoDB...");
+  console.log('Connected to MongoDB...');
 });
 
-console.log("Server running...");
-console.log(bcrypt.hashSync("password123", 12));
+console.log('Server running...');
+console.log(bcrypt.hashSync('password123', 12));
